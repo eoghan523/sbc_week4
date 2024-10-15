bd08761ae1d560032240a13acdf5584e17f18d86
eoghan523 <eoghan523@gmail.com>
2024-10-15 05:11:30 +0100

    My code so far. 50% done. added to script.js.

--- week-4/W4-S3-TheDOM/challenge/assets/js/script.js
+++ week-4/W4-S3-TheDOM/challenge/assets/js/script.js
@@ -1,45 +1,104 @@
-//Create and define the to-do list elements with document.getElementById()
+//DOM waits for the document  to load before starting the function 
+document.addEventListener("DOMContentLoaded", function () {
+   
+    //Creates the Variables and define the to-do list elements with document.getElementById()
 
-const addButton = document.getElementById("addTask");
-const taskInput = document.getElementById("taskInput");
-const taskTableBody =document.getElementById("taskTableBody");
+    const addButton = document.getElementById("addTask");
+    const taskInput = document.getElementById("taskInput");
+    const taskTableBody = document.getElementById("taskTableBody");
+    
+    addButton.addEventListener('click', function() {
 
-
-//This line checks if all the necessary elements are found acoss the document
-if (addTask && taskInput && taskTableBody) {
-
-function addTask() {
-    const task = taskInput.value.trim ();
-
-    if(task) {
-        createTaskElment(task);
-        taskInput.value = '';
-        saveTask();
-    } else {
-        alert("Please add a task!");
-    }
-//This function adds an if else condition. If avalue is entered to the task input it will log and save task. else it will populate an alert message..
-}
-}
-
-addButton.addEventListener('click', function()) {
-    const newTask = taskInput.value.trim();
-
-    if(newTask) {
-        const date =  Date().toLocaleDateString();
-
-        const task = { 
-            status: "Incomplete",
-            Description: 'New Task',
+        const taskToWrite = taskInput.value.trim();
+    
+        const date = new Date().toLocaleDateString();
+    
+        const task = {
+            status: 'Incomplete',
+            description: taskToWrite,
             date: date
-
         };
-    }
-addTaskToTaskTable(task);
+    //Calling function 'addTaskToTable' to add the object of 'task' to the table for reference later.
+        addTaskToTaskTable(task)
+        taskInput.value = '';
+        // Clears input field after adding a task
+    
+        function addTaskToTaskTable(task) {
+            const createNewTask = document.createElement('tr');
+            //creating new variable 'createNewTask', then it gives value which it creates a new element of tr.
+            
+            //.innerHTML grabs the children of create new task which has the value of tr, which is td .
+            createNewTask.innerHTML = `
+                <td>${task.status}</td>
+                <td>${task.description}</td>
+                <td>${task.date}</td>
+                <td>
+                    <button class='completeBtn'>Complete</button>
+                    <button class='completeBtn'>Complete</button>
+                </td>
+            `;
+            taskTableBody.appendChild(createNewTask);
+            
+            // The back ticks create a  template literal which allows you to put multiple lines of code to build the table.            
+        }
+    });
 
-taskInput.value = "";
+})
+
+   
 
 
-}
-//This addEventListiner will add a triggerable event when you click the add Button.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function addTask() {
+//         const task = taskInput.value.trim ();
+
+//         if(task) {
+//             createTaskElment(task);
+//             taskInput.value = '';
+//             saveTask();
+//         } else {
+//             alert("Please add a task!");
+//         }
+//     //This function adds an if else condition. If avalue is entered to the task input it will log and save task. else it will populate an alert message..
+//     }
+
+
+//     addButton.addEventListener('click', function() {
+//         const newTask = taskInput.value.trim();
+
+//         if(newTask) {
+          
+
+//                 task = { 
+//                     status: "Incomplete",
+//                     Description: 'New Task',
+//                     date: date
+//                 };
+            
+//         };
+//     });
+//     addTaskToTaskTable(task);
+
+//     taskInput.value = "";
+
+
+//     });
+//     //This addEventListiner will add a triggerable event when you click the add Button.
+
+
+    
 
